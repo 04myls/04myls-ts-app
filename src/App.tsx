2d35fc5b9b0dc1553a0c8df6694d6cf763009937
04myls <04myls@MacBook-Pro.local>
2023-07-02 02:04:40 +0900

    修正前

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// 今まで学んできたことを活かして簡易Todoリストを作成する
+import { useState } from "react";
+import { TaskList } from "./TaskList";
 
-function App() {
+export type Todo = {
+  id: number;
+  text: string;
+};
+
+const Example: React.FC = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, text: "Todo1" },
+    { id: 2, text: "Todo2" },
+  ]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleClick = () => {
+    setTodos([...todos, { id: todos.length + 1, text: inputText }]);
+    setInputText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={inputText} onChange={handleChange} />
+      <button type="button" onClick={handleClick}>
+        追加
+      </button>
+      <TaskList todos={todos} />
     </div>
   );
-}
+};
 
-export default App;
+export default Example;
